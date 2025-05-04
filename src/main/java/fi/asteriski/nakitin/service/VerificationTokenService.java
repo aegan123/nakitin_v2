c4706344c428df471e7fbab05b1aef16eb88eb31
Juhani Vähä-Mäkilä <juhani@fmail.co.uk>
2025-05-04 22:24:06 +0300

    Make verification token expiry time configurable instead of hard coding a value

--- src/main/java/fi/asteriski/nakitin/service/VerificationTokenService.java
+++ src/main/java/fi/asteriski/nakitin/service/VerificationTokenService.java
@@ -4,23 +4,24 @@ Licenced under EUPL-1.2 or later.
  */
 package fi.asteriski.nakitin.service;
 
-import static fi.asteriski.nakitin.utils.Constants.TOKEN_EXPIRY_HOURS;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 public class VerificationTokenService {
+    @Value("${security.verificationToken.expiryHours}")
+    private Long tokenExpiryHours;
 
     public String generateVerificationToken() {
         return UUID.randomUUID().toString();
     }
 
     public LocalDateTime calculateExpiryDate() {
-        return LocalDateTime.now().plusHours(TOKEN_EXPIRY_HOURS);
+        return LocalDateTime.now().plusHours(tokenExpiryHours);
     }
 
     public boolean isTokenExpired(LocalDateTime expiryDate) {
